d8013f5b189c7e0367be24582f32c345ac0e3fe0
Rakib Hasan <rakibnkhadija@gmail.com>
2025-04-10 16:53:38 +0600

    return the last position before marge habib bro

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router'
+import { router } from './Routes/router_TEMP'
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
 
