2fa5a61582c0ee2872d9ac269bdb2ac777e213f7
TahmidTausif <tahmidtausif49@gmail.com>
2025-04-11 00:43:20 +0600

    update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://bike-museum-server.vercel.app/api',
+    baseUrl: ' https://bike-museum-server-tan.vercel.app/api',
   }),
   tagTypes: ['Product'],
   endpoints: () => ({}),
@@ -12,4 +12,4 @@ export const baseApi = createApi({
 
 
 //  http://localhost:5000/api
-//  https://bike-museum-server.vercel.app/api
+//  https://bike-museum-server-tan.vercel.app/api
