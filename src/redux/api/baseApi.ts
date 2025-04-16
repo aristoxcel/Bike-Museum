ab47bd64204d43da8c084aafc7802a1c5f55600a
TahmidTausif <tahmidtausif49@gmail.com>
2025-04-17 03:58:55 +0600

    update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { RootState } from '../store';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api', 
+    baseUrl: 'https://bike-museum-server-tan.vercel.app/api', 
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
